ce6756e90aa61c432aa5ed3e3f132ac48d665b0d
Enes Baytemir <enesbytmr@gmail.com>
2022-07-18 13:04:18 +0300

    adding source files and pseudo

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Rock Paper Scissors
+we need 3 button for client
+if client click one of them 
+
+the game was start 
+one function random click to one of them 
+
+the counter will count the score
+
+
+
+
+
+*/
